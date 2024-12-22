00a0dfdb144ebb6c13a0acbf51808ff97cd766b7
yecohe <cohen.ye@gmail.com>
2024-12-22 16:08:19 +0200

    Create filter_tool.py

---
+++ filter_tool.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def run():
+    st.subheader("Automatic Filter Tool")
+    st.write("This tool automatically filters URLs based on predefined criteria.")
+    st.checkbox("Enable automatic filtering")
