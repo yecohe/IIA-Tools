8cc0c70875eeb067adb1a6170294e393049a25ff
yecohe <cohen.ye@gmail.com>
2024-12-24 13:35:29 +0200

    Update searching.py

--- searching.py
+++ searching.py
@@ -142,7 +142,6 @@ def check_and_add_headers(sheet):
 
 # Main function to process keywords and URLs
 def process_keywords(client, sheet_id, keywords, lang="en", inurl=False, limit=100):
-    query_sheet = client.open_by_key(sheet_id).worksheet("Keywords")
     keywords_sheet = client.open_by_key(st.secrets["keywords_id"]).worksheet("Keywords")
     sure_sheet = client.open_by_key(sheet_id).worksheet("Sure")
     not_sure_sheet = client.open_by_key(sheet_id).worksheet("Not Sure")
