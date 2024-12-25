91a70f5d2b3a295fea4575fb59e86709174135f4
yecohe <cohen.ye@gmail.com>
2024-12-25 11:31:37 +0200

    Update searching.py

--- searching.py
+++ searching.py
@@ -4,7 +4,7 @@ import pycld2 as cld2
 import re
 from googletrans import Translator
 from collections import Counter
-#from googlesearch import search
+from googlesearch import search
 from datetime import datetime
 import pytz
 import streamlit as st
