ea389add8d91dacfdeac301aae14b3c84802a483
yecohe <cohen.ye@gmail.com>
2025-01-01 15:28:00 +0200

    Update searching.py

--- searching.py
+++ searching.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pycld2 as cld2
 import re
-#from googletrans import Translator
+from googletrans import Translator
 from collections import Counter
 from datetime import datetime
 import pytz
